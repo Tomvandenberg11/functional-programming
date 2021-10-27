55bdace111b93b1269ddbab09fd4d7004f992d89
Tom van den Berg <tomvandenberg11@hotmail.com>
2021-10-27 13:10:51 +0200

    adding rgb colors with switch and colorString package to homepage and making it background of div

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const _ = require('lodash')
 const express = require('express')
 const app = express()
+const colorString = require('color-string')
 
 const port = 3000;
 
@@ -14,13 +15,46 @@ function main() {
   const eyeColor = 'Wat is je oogkleur?'
 
   const color = jsonFile.map(answer =>
-    _.capitalize(answer[eyeColor])
+    answer[eyeColor].toLowerCase()
   )
+
+  const getColor = function (color) {
+    switch (color) {
+      case 'bruin':
+        return 'brown'
+      case 'blauw':
+        return 'deepskyblue'
+      case 'grijs':
+        return 'gray'
+      case 'groen':
+        return 'green'
+      case 'donkerbruin':
+        return 'saddlebrown'
+      case 'groen - grijs':
+        return 'darkseagreen'
+      case 'groen-grijs':
+        return 'darkseagreen'
+      case 'groen-blauw':
+        return 'mediumaquamarine'
+    }
+  }
+
+  const arrayLength = color.length;
+
+  function translateColor(item) {
+    return colorString.get.rgb(getColor(item))
+  }
+
+  const rgb = []
+
+  for ( let i = 0; i < arrayLength; i++) {
+    rgb.push(translateColor(color[i]))
+  }
 
   console.log(color)
 
   app.get('/', (req, res) => res.render('index.ejs', {
-    data: color,
+    data: rgb,
   }))
 
   app.listen(port, () => {
